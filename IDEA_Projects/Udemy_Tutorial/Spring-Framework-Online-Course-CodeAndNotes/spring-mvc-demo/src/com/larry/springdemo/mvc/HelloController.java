f1d0d26f6af8e1010ab3c24dde8431b1e62d0ea3
Larry Jiang <larry.hangyan@gmail.com>
2017-06-10 22:23:46 +0800

    spring-mvc frame

---
+++ IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/spring-mvc-demo/src/com/larry/springdemo/mvc/HelloController.java
@@ -0,0 +1,16 @@
+package com.larry.springdemo.mvc;
+
+/**
+ * Created by hangyanjiang on 2017/6/10.
+ */
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("/")
+    public String showPage() {
+        return null;
+    }
+
+}
