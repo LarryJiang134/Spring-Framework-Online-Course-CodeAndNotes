804fb24265f273ab0418e5e194611fc1b670502e
Larry Jiang <larry.hangyan@gmail.com>
2017-06-11 21:32:30 +0800

    spring-mvc-test-field

---
+++ IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/springmvc-demo/src/main/java/com/larry/spring/mvc/Student.java
@@ -0,0 +1,31 @@
+package com.larry.spring.mvc;
+
+/**
+ * Created by hangyanjiang on 2017/6/11.
+ */
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/springmvc-demo/src/main/webapp/WEB-INF/jsp/main-menu.jsp
+++ IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/springmvc-demo/src/main/webapp/WEB-INF/jsp/main-menu.jsp
@@ -10,6 +10,10 @@
 
 <a href="hello/showForm">Hello World form</a>
 
+<br><br>
+
+<a href="/student/showForm">Student form</a>
+
 </body>
 
 </html>
