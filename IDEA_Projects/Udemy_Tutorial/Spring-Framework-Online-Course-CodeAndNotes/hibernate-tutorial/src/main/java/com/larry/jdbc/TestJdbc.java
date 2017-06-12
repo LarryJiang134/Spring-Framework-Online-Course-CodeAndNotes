07336303ff1b3ef3f392bd95105523b21d355b33
Larry Jiang <larry.hangyan@gmail.com>
2017-06-12 15:11:46 +0800

    hibernate-setup

---
+++ IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/hibernate-tutorial/src/main/java/com/larry/jdbc/TestJdbc.java
@@ -0,0 +1,33 @@
+package com.larry.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+/**
+ * Created by hangyanjiang on 2017/6/12.
+ */
+public class TestJdbc {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?user55L=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+
+            System.out.println(">> Connecting to database: " + jdbcUrl);
+
+            Connection myConn =
+                    DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println(">> Connection successful");
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+    }
+
+}
