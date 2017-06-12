f5f24cac4d5d1dd85756252b11dc110417027105
Larry Jiang <larry.hangyan@gmail.com>
2017-06-12 10:32:01 +0800

    spring-mvc-validation-trim

--- IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/springmvc-demo/src/main/java/com/larry/spring/mvc/CustomerController.java
+++ IDEA_Projects/Udemy_Tutorial/Spring-Framework-Online-Course-CodeAndNotes/springmvc-demo/src/main/java/com/larry/spring/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.larry.spring.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +18,19 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add aan initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        // true: if all whitespaces trim to null
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
 
     @RequestMapping("showForm")
     public String showForm(Model theModel) {
